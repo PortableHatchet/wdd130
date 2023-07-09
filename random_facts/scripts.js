a04c47b250799716f99fd88bdbca7ed5356fbef5
Porter Hatch <porterhatch1@gmail.com>
2023-07-08 19:43:07 -0600

    created the find-facts page and set up the javascript to grab the random fact

--- random_facts/scripts.js
+++ random_facts/scripts.js
@@ -1,20 +1,12 @@
 function getRandomLineFromFile(fileUrl) {
-  var githubApiUrl = 'https://api.github.com/repos/';
-  var repoOwner = 'PortableHatchet';
-  var repoName = 'wdd130';
-  var filePath = 'wdd130/random_facts/facts.txt';
-  var url = githubApiUrl + repoOwner + '/' + repoName + '/contents/' + filePath;
-
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
-      var response = JSON.parse(this.responseText);
-      var content = atob(response.content);
-      var lines = content.split('\n');
+      var lines = this.responseText.split('\n');
       var randomLine = lines[Math.floor(Math.random() * lines.length)];
       document.getElementById('output').textContent = randomLine;
     }
   };
-  xhttp.open('GET', url, true);
+  xhttp.open('GET', fileUrl, true);
   xhttp.send();
 }
