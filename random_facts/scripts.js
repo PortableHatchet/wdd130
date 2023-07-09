62490feef2196c779bbccf9bef62ecd20b5853b3
Porter Hatch <porterhatch1@gmail.com>
2023-07-08 19:03:53 -0600

    added script for facts and text file

--- random_facts/scripts.js
+++ random_facts/scripts.js
@@ -1,3 +1,20 @@
-function myFunction(x) {
-    x.classList.toggle("fa-thumbs-down");
-  }
+function getRandomLineFromFile(fileUrl) {
+  var githubApiUrl = 'https://api.github.com/repos/';
+  var repoOwner = 'PortableHatchet';
+  var repoName = 'wdd130';
+  var filePath = 'wdd130/random_facts/facts.txt';
+  var url = githubApiUrl + repoOwner + '/' + repoName + '/contents/' + filePath;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      var response = JSON.parse(this.responseText);
+      var content = atob(response.content);
+      var lines = content.split('\n');
+      var randomLine = lines[Math.floor(Math.random() * lines.length)];
+      document.getElementById('output').textContent = randomLine;
+    }
+  };
+  xhttp.open('GET', url, true);
+  xhttp.send();
+}
